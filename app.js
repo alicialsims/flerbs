66e857efcd90caac0bd2689ea29259dbce8cd38d
Alicia Sims <alicialsims@gmail.com>
2017-03-26 23:42:33 -0400

    Angular has a bug in pug.

--- app.js
+++ app.js
@@ -48,8 +48,9 @@ app.listen(8080, ()=> {
 //error handler
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.send(err);
+  //res.render('error', {
+  //  message: err.message,
+  //  error: {}
+  //});
 });
--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,4 +3,10 @@
 const myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http){
 	console.log ('controller is working');
+
+// getting my data from mongo
+$http.get('/flerbs').then((success)=>{
+	console.log('data acquired from mongo');
+});
+
 }]);
